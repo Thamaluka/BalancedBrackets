84f280fb91eed4feff2bad6a3b7376c7387256bc
Thamires <thamires@Diegos-Mac-mini.local>
2019-05-31 11:18:06 -0300

    inicial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+To run the project
+    Open console and run
+    npm install
+    and then npm start and apply your sequence
---
+++ app.js
@@ -0,0 +1,33 @@
+import readline from 'readline';
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.question("Tap a sequece: ", (resp) => {
+    const validation = isValid(resp);
+    console.log("Your sequence is", validation ? "valid" : "invalid")
+    reader.close();
+});
+
+const isValid = (resp) => {
+    let list = [];
+    let simbols = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for (let i = 0; i < resp.length; i++) {
+        if (resp[i] === '(' || resp[i] === '{' || resp[i] === '[') {
+            list.push(resp[i]);
+        } else {
+            let last = list.pop();
+            if (resp[i] !== simbols[last]) { return false };
+        }
+    }
+    if (list.length !== 0) { return false };
+    return true;
+}
+
